e712a98ba24d945186fb5da08626072331fb2cdd
elmunro <anon_fwd@icloud.com>
2021-10-21 11:27:59 +0200

    rm dupes

--- essentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PYTHON="git gcc clang python3 python3-pip python3-setuptools python3-wheel virtualenvwrapper"
-SYSTOOLS="ufw ncdu rkhunter net-tools dnsutils dnsenum traceroute hping3"
-XTOPS="iftop htop virt-top"
-
-apt update 
-apt install -y $PYTHON $SYSTOOLS $XTOPS 
-
